0af3a67c803cf8a39b2bcdbe89dab34a3648dc7e
andreidzuiba <aadzuiba@yandex.ru>
2022-10-09 22:08:43 +0300

    many tiles

--- main_test.go
+++ main_test.go
@@ -12,11 +12,17 @@ import (
 func TestHandler(t *testing.T) {
 	r := placements_request{
 		Id: "780",
-		Tiles: []tiles{tiles{
-			Id:    15,
-			Width: 100,
-			Ratio: 1.5,
-		}},
+		Tiles: []tiles{
+			tiles{
+				Id:    15,
+				Width: 100,
+				Ratio: 1.5,
+			},
+			tiles{
+				Id:    17,
+				Width: 110,
+				Ratio: 1.2,
+			}},
 		Context: context{
 			Ip:         "192.168.10.10",
 			User_agent: "diospiros",
